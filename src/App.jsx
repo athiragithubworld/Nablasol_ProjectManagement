4e474c4e9b370647b58f8600677cbf0ee6ee3b17
athiragithubworld <athirahrms@gmail.com>
2024-06-09 21:58:21 +0530

    App jsx and Index jsx file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import React from "react";
+import ProjectCreation from "./Components/ProjectCreation";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ProjectCreation />
+    </div>
+  );
+}
+
+export default App;
+
