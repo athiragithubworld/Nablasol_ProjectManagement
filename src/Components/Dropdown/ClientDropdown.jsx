4e26d6fe6df098b85296f0435a38a07aac79ff88
athiragithubworld <athirahrms@gmail.com>
2024-06-10 11:44:35 +0530

    Some updates in ClientDropdown

--- src/Components/Dropdown/ClientDropdown.jsx
+++ src/Components/Dropdown/ClientDropdown.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
+
+// ClientDropdown component to select a client from a dropdown list
 const ClientDropdown = ({ value, onChange, clients, addClient }) => {
+  
+  // State to manage the dropdown's open/close status
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="mb-4  w-1/2">
-     
       <div className="relative w-full ">
+        {/* Button to toggle the dropdown menu */}
         <button
           type="button"
           onClick={() => setIsOpen(!isOpen)}
@@ -14,9 +18,11 @@ const ClientDropdown = ({ value, onChange, clients, addClient }) => {
         >
           {value || "Select a client"}
         </button>
+        {/* Dropdown menu */}
         {isOpen && (
           <div className="absolute bg-white border border-gray-300 rounded mt-1 w-full z-10">
             <ul>
+              {/* Map through the clients array to render each client as a list item */}
               {clients.map((client) => (
                 <li
                   key={client}
